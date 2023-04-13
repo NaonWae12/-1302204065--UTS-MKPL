f5b8d52dc0250c07fc11b7460f7188ca0c3af8a0
anwar <saefulanwar7721@gmail.com>
2023-04-13 14:58:36 +0700

    melakukan refactoring terhadap bad smells Long Method

--- src/uts/TaxFunction.java
+++ src/uts/TaxFunction.java
@@ -9,35 +9,51 @@ package uts;
  * @author Saeful anwar
  */
 public class TaxFunction {
-    
+
     /**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-	 * 
-	 */
-	
-    public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+     * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus
+     * dibayarkan setahun.
+     * 
+     * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan
+     * bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan)
+     * dikurangi penghasilan tidak kena pajak.
+     * 
+     * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena
+     * pajaknya adalah Rp 54.000.000.
+     * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah
+     * sebesar Rp 4.500.000.
+     * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya
+     * ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
+     * 
+     */
+
+    private static final int MAX_NUM_CHILDREN = 3;
+    private static final int MARRIED_DEDUCTIBLE = 58500000;
+    private static final int SINGLE_DEDUCTIBLE = 54000000;
+    private static final int CHILD_DEDUCTIBLE = 1500000;
+
+    public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible,
+            boolean isMarried, int numberOfChildren) {
         int tax = 0;
         if (numberOfMonthWorking > 12) {
             System.err.println("More than 12 month working per year");
         }
-        if (numberOfChildren > 3) {
-            numberOfChildren = 3;
-	}
+        numberOfChildren = Math.min(numberOfChildren, MAX_NUM_CHILDREN);
+        int taxableIncome = calculateTaxableIncome(monthlySalary, otherMonthlyIncome, numberOfMonthWorking, deductible,
+                isMarried, numberOfChildren);
+        tax = (int) Math.round(0.05 * taxableIncome);
+        return Math.max(tax, 0);
+    }
+
+    private static int calculateTaxableIncome(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking,
+            int deductible, boolean isMarried, int numberOfChildren) {
+        int taxableIncome = ((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible;
         if (isMarried) {
-            tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-        }else {
-            tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
+            taxableIncome -= MARRIED_DEDUCTIBLE;
+        } else {
+            taxableIncome -= SINGLE_DEDUCTIBLE;
         }
-        if (tax < 0) {
-            return 0;
-        }else {
-            return tax;
-        }
+        taxableIncome -= (numberOfChildren * CHILD_DEDUCTIBLE);
+        return taxableIncome;
     }
 }
